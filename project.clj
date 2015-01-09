a1f27225dc923a040eb1948d66d6f93b3877e07e
Nils Grunwald <nils@grunwald.fr>
2015-01-09 18:58:30 +0100

    added uri to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-mapdb "0.1.0-SNAPSHOT"
   :description "Idiomatic Clojure wrapper around MapDB"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/ngrunwald/clj-mapdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
