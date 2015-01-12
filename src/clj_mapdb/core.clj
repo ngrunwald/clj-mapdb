103f8947d5891577caa5cd6a57c90222c9d95045
Nils Grunwald <nils@grunwald.fr>
2015-01-12 15:06:34 +0100

    removed higher level constructs

--- src/clj_mapdb/core.clj
+++ src/clj_mapdb/core.clj
@@ -1,7 +1,6 @@
 (ns clj-mapdb.core
   (:import [org.mapdb DBMaker DB])
   (:require [clojure.java.io :as io]
-            [potemkin [types :refer [definterface+]]]
             [clojure.string :as str]
             [iroh.core :as iroh :refer [.?]]))
 
@@ -18,8 +17,10 @@
   [s]
   (-> s
       (str/trim)
-      (str/replace #"([a-z])([A-Z]{1,}?)([A-Z])([a-z])" (fn [[_ a b c d]] (str a "-" (str/lower-case b) "-" (str/lower-case c) d)))
-      (str/replace #"([A-Z])([a-z])" (fn [[_ a b]] (str "-" (str/lower-case a) b)))))
+      (str/replace #"([a-z])([A-Z]{1,}?)([A-Z])([a-z])"
+                   (fn [[_ a b c d]] (str a "-" (str/lower-case b) "-" (str/lower-case c) d)))
+      (str/replace #"([A-Z])([a-z])"
+                   (fn [[_ a b]] (str "-" (str/lower-case a) b)))))
 
 (defn get-configure-methods
   [klass]
@@ -74,64 +75,16 @@
         (recur (rest todo)))
       maker)))
 
-(definterface+ IMapDB
-  (db        [this] "Returns the underlying db")
-  (close!    [this] "Closes this db")
-  (closed?   [this] "Returns whether this db is closed")
-  (commit!   [this] "Commits all pending changes on this db")
-  (rollback! [this] "Rollbacks all pending changes on this db")
-  (compact!  [this] "Compacts this DB to reclaim space")
-  (rename!   [this old-name new-name] "Renames this collection")
-  (snapshot  [this] "Returns a read-only snapshot view of this db")
-  (storage   [this] "Returns the storage type for this db")
-  (options   [this] "Returns the options this db was created with"))
-
 (defn create-collection!
   [mdb collection-type label opts]
-  (if-let [old-coll (get mdb (name label))]
+  (if-let [old-coll (.get mdb (name label))]
     old-coll
     (let [{:keys [ctor options]} (get coll-types collection-type)
-          maker (ctor (db mdb) label)]
+          maker (ctor mdb label)]
       (configure-maker! options maker opts)
       (.make maker))))
 
-(deftype MapDB [db storage options]
-  IMapDB
-  (db        [this] db)
-  (close!    [this] (.close db) this)
-  (closed?   [this] (.isClosed db))
-  (commit!   [this] (.commit db) this)
-  (rollback! [this] (.rollback db) this)
-  (compact!  [this] (.compact db) this)
-  (storage   [this] storage)
-  (options   [this] options)
-  clojure.lang.Counted
-  (count [this]
-    (count (.getAll db)))
-  clojure.lang.ILookup
-  (valAt [this k] (.get db (name k)))
-  (valAt [this k opts] (create-collection! this (:type opts) (name k) opts))
-  clojure.lang.ITransientMap
-  (assoc [this k opts] (create-collection! this (:type opts) (name k) opts) this)
-  (without [this k] (.delete db (name k)) this)
-  clojure.lang.Seqable
-  (seq [this]
-    (seq (.getAll db)))
-  clojure.lang.IFn
-  (invoke [this k]
-    (get this (name k))))
-
-(defmethod print-method MapDB
-  [db ^java.io.Writer w]
-  (.write w "#MapDB<")
-  (.write w (name (storage db)))
-  (.write w ">{")
-  (let [all-colls (for [[k v] db]
-                    (str k " <" (last (str/split (str (type v)) #"\.")) ">"))]
-    (.write w (str/join ", " all-colls)))
-  (.write w "}"))
-
-(defn mapdb
+(defn create-db
   ([db-type arg opts]
    (let [{:keys [ctor args]} (mapdb-types (keyword db-type))
          arity (count args)
@@ -142,7 +95,7 @@
                    (ctor arg))
                  (ctor))]
      (configure-maker! db-maker-options maker opts)
-     (->MapDB (.make maker) db-type opts)))
-  ([db-type other] (if (map? other) (mapdb db-type nil other) (mapdb other {})))
-  ([db-type] (mapdb db-type nil {}))
-  ([] (mapdb :heap nil {})))
+     (.make maker)))
+  ([db-type other] (if (map? other) (create-db db-type nil other) (create-db other {})))
+  ([db-type] (create-db db-type nil {}))
+  ([] (create-db :heap nil {})))
