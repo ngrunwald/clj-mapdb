373b60292c599ad7bef9347877999d56d31aae35
Nils Grunwald <nils@grunwald.fr>
2015-12-15 10:31:26 +0100

    bump version and release

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject clj-mapdb "0.1.0-beta2-SNAPSHOT"
+(defproject clj-mapdb "0.1.0"
   :description "Idiomatic Clojure wrapper around MapDB"
   :url "https://github.com/ngrunwald/clj-mapdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.mapdb/mapdb "1.0.7"]
-                 [potemkin "0.3.11"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.mapdb/mapdb "1.0.8"]
+                 [potemkin "0.4.2"]
                  [im.chit/iroh "0.1.11"]]
-  :profiles {:dev {:dependencies [[expectations "2.0.13"]]
+  :profiles {:dev {:dependencies [[expectations "2.1.4"]]
                    :plugins [[lein-expectations "0.0.7"]]
                    :aot [clj-mapdb.serializers]}})
